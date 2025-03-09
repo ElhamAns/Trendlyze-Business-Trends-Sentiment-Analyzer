665f2295c4a3047e603350dbcc905e3b8b8a4f9f
seniorprojectbtsa@gmail.com <seniorprojectbtsa@gmail.com>
2025-03-09 10:53:32 +0000

    Edited admin_dashboard.html and admin_dashboard

--- client_code/admin_dashboard/__init__.py
+++ client_code/admin_dashboard/__init__.py
@@ -5,11 +5,12 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-
+from ..material_icons import *
 
 class admin_dashboard(admin_dashboardTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    
 
     # Any code you write here will run when the form opens.
