d7b947a7c09dbc6bae0d645efa9f2cd68325727b
seniorprojectbtsa@gmail.com <seniorprojectbtsa@gmail.com>
2025-03-09 11:51:48 +0000

    Edited admin_dashboard

--- client_code/admin_dashboard/__init__.py
+++ client_code/admin_dashboard/__init__.py
@@ -5,7 +5,6 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-from ..material_icons import *
 
 class admin_dashboard(admin_dashboardTemplate):
   def __init__(self, **properties):
