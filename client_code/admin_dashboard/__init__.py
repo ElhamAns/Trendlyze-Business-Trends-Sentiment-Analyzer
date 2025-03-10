20370af848c7f021ede31887bd2dcf8c2b2fb5e2
seniorprojectbtsa@gmail.com <seniorprojectbtsa@gmail.com>
2025-03-10 04:26:58 +0000

    Edited admin_dashboard.RowTemplate2 and admin_dashboard

--- client_code/admin_dashboard/__init__.py
+++ client_code/admin_dashboard/__init__.py
@@ -10,6 +10,7 @@ class admin_dashboard(admin_dashboardTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.repeating_panel_3.items = ['test']
     
 
     # Any code you write here will run when the form opens.
