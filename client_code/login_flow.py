00e4707b18c8935785e048c0e3275ec46cbd66ff
seniorprojectbtsa@gmail.com <seniorprojectbtsa@gmail.com>
2025-03-15 00:48:40 +0000

    Edited login_flow

--- client_code/login_flow.py
+++ client_code/login_flow.py
@@ -36,5 +36,6 @@ def do_email_confirm_or_reset():
     elif 'confirm' in h:
       if anvil.server.call('_confirm_email_address', h['email'], h['confirm']):
         alert("Thanks for confirming your email address. You are now logged in.")
+        
       else:
         alert("This confirmation link is not valid. Perhaps you have already confirmed your address?\n\nTry logging in normally.")
