b1c52790bf99105898cf4b59a42b440ccbceb32f
seniorprojectbtsa@gmail.com <seniorprojectbtsa@gmail.com>
2025-03-15 00:43:42 +0000

    Edited login_flow

--- client_code/login_flow.py
+++ client_code/login_flow.py
@@ -5,6 +5,7 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+from .PasswordResetDialog import PasswordResetDialog
 
 def do_email_confirm_or_reset():
   print("herereeeeeee")
