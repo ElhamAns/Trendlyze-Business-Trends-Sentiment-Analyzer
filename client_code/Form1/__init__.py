1667fcd9cd6aef421b3a60c63416344248c3e756
seniorprojectbtsa@gmail.com <seniorprojectbtsa@gmail.com>
2025-04-10 03:59:36 +0000

    Edited theme.css and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -16,11 +16,11 @@ class Form1(Form1Template):
     # Any code you write here will run when the form opens.
   def button_7_click(self, **event_args):
     """This method is called when the button is clicked"""
-    pass
+    self.column_panel_3.scroll_into_view(smooth=True)
 
   def button_6_click(self, **event_args):
     """This method is called when the button is clicked"""
-    pass
+    self.label_5.scroll_into_view(smooth=True)
 
   def button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
@@ -31,4 +31,16 @@ class Form1(Form1Template):
     """This method is called when the button is clicked"""
     open_form('login')
     pass
+
+  def button_5_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    self.card_1.scroll_into_view(smooth=True)
+
+  def button_4_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    self.label_12.scroll_into_view(smooth=True)
+
+  def button_3_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    self.label_24.scroll_into_view(smooth=True)
 
