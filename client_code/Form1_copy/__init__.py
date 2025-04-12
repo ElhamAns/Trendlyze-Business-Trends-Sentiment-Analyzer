376ad94b5639e1ed1748fd56e17ff3b6c3a0ee09
seniorprojectbtsa@gmail.com <seniorprojectbtsa@gmail.com>
2025-04-12 10:30:53 +0000

    Created Form1_copy_copy and Form1_copy; Edited settings, authh_module and Form1

--- client_code/Form1_copy/__init__.py
+++ client_code/Form1_copy/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import Form1_copyTemplate
 from anvil import *
 import anvil.server
 import anvil.tables as tables
@@ -6,34 +6,34 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.users
 
-from ..Form1_copy import Form1_copy
-
 from anvil_extras import routing
 
-@routing.main_router
-class Form1(Form1Template):
+@routing.route('home', title='Home | RoutingExample')
+@routing.route('',     title='Home | RoutingExample')
+class Form1_copy(Form1_copyTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
     self.links = [self.button_2, self.button_1]
-    self.button_7.tag.url_hash  = 'home'
-    self.button_2.tag.url_hash = 'login'
-    self.button_1.tag.url_hash   = 'register'
+    # self.home_link.tag.url_hash       = 'home'
+    self.button_2.tag.url_hash = "login"
+    self.button_1.tag.url_hash = "register"
 
   def nav_link_click(self, **event_args):
     """This method is called when a navigation link is clicked"""
-    url_hash = event_args['sender'].tag.url_hash
+    url_hash = event_args["sender"].tag.url_hash
     routing.set_url_hash(url_hash)
 
   def on_navigation(self, **nav_args):
     # this method is called whenever routing navigates to a new url
-    # an example of setting the link as selected 
+    # an example of setting the link as selected
     # depending on the url_hash that is being navigated to
     for link in self.links:
-      link.role = 'selected' if link.tag.url_hash == nav_args['url_hash'] else ''
+      link.role = "selected" if link.tag.url_hash == nav_args["url_hash"] else ""
 
     # Any code you write here will run when the form opens.
-  def home_link_click(self, **event_args):
+
+  def button_7_click(self, **event_args):
     """This method is called when the button is clicked"""
     self.column_panel_3.scroll_into_view(smooth=True)
 
@@ -43,12 +43,12 @@ class Form1(Form1Template):
 
   def button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
-    open_form('register')
+    open_form("register")
     pass
 
   def button_2_click(self, **event_args):
     """This method is called when the button is clicked"""
-    open_form('login')
+    open_form("login")
     pass
 
   def button_5_click(self, **event_args):
@@ -62,4 +62,3 @@ class Form1(Form1Template):
   def button_3_click(self, **event_args):
     """This method is called when the button is clicked"""
     self.label_24.scroll_into_view(smooth=True)
-
--- client_code/authh_module.py
+++ client_code/authh_module.py
@@ -5,9 +5,10 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 from anvil import open_form
 from . import login_flow
+from .Form1 import Form1
 
 from anvil_extras import routing
 
 routing.launch()
-login_flow.do_email_confirm_or_reset()
-open_form('Form1')
+# login_flow.do_email_confirm_or_reset()
+# open_form('Form1')
