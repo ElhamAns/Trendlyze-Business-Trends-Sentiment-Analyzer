e52f1ac7af4c9fb193f0d7a493f9bbdd60603d7d
seniorprojectbtsa@gmail.com <seniorprojectbtsa@gmail.com>
2025-04-19 14:18:27 +0000

    Edited PrivacyPolicy, settings and ServicePage

--- client_code/PrivacyPolicy/__init__.py
+++ client_code/PrivacyPolicy/__init__.py
@@ -5,11 +5,26 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+from anvil_extras import routing
+
 
 
 class PrivacyPolicy(PrivacyPolicyTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.button_7.tag.url_hash  = 'home'
+    self.button_2.tag.url_hash = 'login'
+    self.button_6.tag.url_hash = 'home'
+    self.button_1.tag.url_hash   = 'register'
 
     # Any code you write here will run when the form opens.
+
+  def nav_link_click(self, **event_args):
+    """This method is called when a navigation link is clicked"""
+    url_hash = event_args['sender'].tag.url_hash
+    routing.set_url_hash(url_hash)
+
+  def on_navigation(self, **nav_args):
+    for link in self.links:
+      link.role = 'selected' if link.tag.url_hash == nav_args['url_hash'] else ''
--- client_code/ServicePage/__init__.py
+++ client_code/ServicePage/__init__.py
@@ -5,6 +5,7 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+from anvil_extras import routing
 
 
 class ServicePage(ServicePageTemplate):
@@ -13,3 +14,13 @@ class ServicePage(ServicePageTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run when the form opens.
+
+  def nav_link_click(self, **event_args):
+    """This method is called when a navigation link is clicked"""
+    url_hash = event_args['sender'].tag.url_hash
+    routing.set_url_hash(url_hash)
+
+  def on_navigation(self, **nav_args):
+    for link in self.links:
+      link.role = 'selected' if link.tag.url_hash == nav_args['url_hash'] else ''
+
