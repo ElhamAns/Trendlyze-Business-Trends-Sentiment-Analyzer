8e1a9edaaf1532797e15756a2e3c8943fde3b272
seniorprojectbtsa@gmail.com <seniorprojectbtsa@gmail.com>
2025-04-22 03:10:50 +0000

    Edited PrivacyPolicy and PaymentForm

--- client_code/PrivacyPolicy/__init__.py
+++ client_code/PrivacyPolicy/__init__.py
@@ -8,17 +8,17 @@ from anvil.tables import app_tables
 from anvil_extras import routing
 
 
-
+@routing.route('privacyPolicy', title="BusinessTrend")
 class PrivacyPolicy(PrivacyPolicyTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.links = [self.button_2, self.button_1, self.button_6]
     self.button_7.tag.url_hash  = 'home'
     self.button_2.tag.url_hash = 'login'
     self.button_6.tag.url_hash = 'home'
     self.button_1.tag.url_hash   = 'register'
 
-    # Any code you write here will run when the form opens.
 
   def nav_link_click(self, **event_args):
     """This method is called when a navigation link is clicked"""
@@ -28,3 +28,13 @@ class PrivacyPolicy(PrivacyPolicyTemplate):
   def on_navigation(self, **nav_args):
     for link in self.links:
       link.role = 'selected' if link.tag.url_hash == nav_args['url_hash'] else ''
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('register')
+    pass
+
+  def button_2_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('login')
+    pass
